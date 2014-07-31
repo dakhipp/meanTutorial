ad495b6c7d4b545bf35fd1f906005f3346550804
dakota hipp <dakhipp@dakBook.local>
2014-07-30 19:16:50 -0700

    tutoral skeloton complete, angular fixed with cdn

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
 	stylus = require('stylus'),
 	logger = require('morgan'),
-	bodyParser = require('body-parser');
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'develpoment';
 
@@ -24,12 +25,35 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public')); 
 
-app.get('/partials/:partialPath', function(req, res){
-	res.render('partials/'+req.params.partialPath);
+mongoose.connect('mongodb://localhost/meanTutorial');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback(){
+ 	console.log('Mean Tutorial db opened');
+
+ });
+
+var Schema = mongoose.Schema;
+var messageSchema = new Schema({
+	message: String
+});
+var Message = mongoose.model('Message', messageSchema);
+
+var mongoMessage;
+Message.findOne(function (err, messageDoc) {
+  console.log(messageDoc);
+  mongoMessage = messageDoc.message;
 });
 
+app.get('/partials/:partialPath', function(req, res){
+	res.render('partials/'+req.params.partialPath);
+})
+
 app.get('*', function(req, res){
-	res.render('index');
+	res.render('index', {
+		mongoMessage : mongoMessage
+	});
 });
 
 var port = 3030;
